5b19424f4b4539d74c66bba20614773328126774
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-05 20:57:54 -0300

    Add relationships and fillable fields to Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
-    //
+    protected $fillable = ['name', 'cnpj'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function clients(): BelongsToMany
+    {
+        return $this->belongsToMany(Client::class);
+    }
 }
