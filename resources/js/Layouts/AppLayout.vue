b01f4ad63055cd21d91afcc1ddae8036a361b2e1
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-05 22:39:59 -0300

    Add ThemeSwitcher component to AppLayout for dark mode integration

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import ThemeSwitcher from '@/Components/ThemeSwitcher.vue';
 
 defineProps({
     title: String,
@@ -58,6 +59,7 @@ const logout = () => {
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
+                            <ThemeSwitcher />
                             <div class="ms-3 relative">
                                 <!-- Teams Dropdown -->
                                 <Dropdown v-if="$page.props.jetstream.hasTeamFeatures" align="right" width="60">
