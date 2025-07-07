6bff1c0d02c942915bb4d8ae88975b7259d92619
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-07 12:11:44 -0300

    Add Tooltip component with tippy.js integration for enhanced tooltips

---
+++ resources/js/Components/Tooltip.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/themes/light-border.css'
+
+const props = defineProps({
+    content: {
+        type: String,
+        required: true,
+    },
+    placement: {
+        type: String,
+        default: 'top',
+    },
+    theme: {
+        type: String,
+        default: 'light-border',
+    },
+    animation: {
+        type: String,
+        default: 'shift-away',
+    },
+})
+
+const el = ref(null)
+
+onMounted(() => {
+    tippy(el.value, {
+        content: props.content,
+        placement: props.placement,
+        theme: props.theme,
+        animation: props.animation,
+        arrow: true,
+        delay: [100, 50],
+    })
+})
+</script>
+
+<template>
+  <span ref="el">
+    <slot />
+  </span>
+</template>
