ef1519cd5919458564af4091fbfc69ba90def2b3
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-08 14:19:22 -0300

    Add migration for sales and installments tables

---
+++ database/migrations/2025_07_08_160544_create_sales_and_installments_tables.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->decimal('total', 10, 2);
+            $table->enum('payment_type', ['cash', 'installment']);
+            $table->unsignedTinyInteger('installments_count')->nullable(); // Apenas para parcelado
+            $table->timestamps();
+        });
+
+        Schema::create('installments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained()->onDelete('cascade');
+            $table->unsignedTinyInteger('number'); // Ex: 1ª, 2ª, 3ª
+            $table->decimal('amount', 10, 2);
+            $table->date('due_date');
+            $table->date('paid_at')->nullable(); // nulo = não paga
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('installments');
+        Schema::dropIfExists('sales');
+    }
+};
