eccbdcb933b2594fdefb2f77a954ae66d04d4331
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-09 10:39:30 -0300

    Update AppLayout title to 'Clientes'

--- resources/js/Pages/Client/Index.vue
+++ resources/js/Pages/Client/Index.vue
@@ -64,7 +64,7 @@ watch(search, (value) => applySearch(value))
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="Clientes">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Clientes
