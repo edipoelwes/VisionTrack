bedf8338b0e8e22fc46f40c2ce196e38b3451a5c
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-09 18:53:26 -0300

    Add entry_value display to SaleSummary.vue

--- resources/js/Pages/Sale/Partials/SaleSummary.vue
+++ resources/js/Pages/Sale/Partials/SaleSummary.vue
@@ -22,10 +22,14 @@ const props = defineProps({
             </div>
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
             <div>
                 <p class="text-sm text-gray-500 dark:text-gray-400">Valor Total</p>
                 <p class="text-lg font-semibold text-gray-800 dark:text-white">{{ formatCurrencyBR(sale.total) }}</p>
+            </div>
+            <div>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Valor da Entrada</p>
+                <p class="text-lg font-semibold text-gray-800 dark:text-white">{{ formatCurrencyBR(sale.entry_value) }}</p>
             </div>
             <div>
                 <p class="text-sm text-gray-500 dark:text-gray-400">Data da Venda</p>
