c1d3825e00cafce7e072874e9b77b236e53005d2
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-16 08:24:42 -0300

    Automatically seed the database if no companies exist during boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Company;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!Company::query()->exists()) {
+            Artisan::call('db:seed');
+        }
     }
 }
