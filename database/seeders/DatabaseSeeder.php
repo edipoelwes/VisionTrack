fc553b4cd594713af285597e7c4fb7095ef2bcb8
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-16 09:56:51 -0300

    Comment out CompanySeeder call in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CompanySeeder::class,
+//            CompanySeeder::class,
 //            ClientSeeder::class,
 //            PrescriptionSeeder::class,
 //            AddressSeeder::class,
