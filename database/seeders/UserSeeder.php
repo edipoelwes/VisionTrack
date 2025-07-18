9b8aed2e994cd98bb6f6ca45182bdb64983c5846
Edipo Elwes <edipoelwes2@gmail.com>
2025-07-17 21:02:56 -0300

    Refactor UserSeeder to use query method and add password and email verification

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,16 +13,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::query()->create([
             'name' => 'Edipo Elwes',
             'email' => 'edipoelwes2@gmail.com',
+            'password' => Hash::make('Teste@123'),
+            'email_verified_at' => now(),
             'company_id' => 1,
         ]);
 
-        User::factory()->create([
+        User::query()->create([
             'name' => 'Hitalo Vieira',
             'email' => 'hit_alo_santos@hotmail.com',
             'password' => Hash::make('Jessielly'),
+            'email_verified_at' => now(),
             'company_id' => 1,
         ]);
     }
